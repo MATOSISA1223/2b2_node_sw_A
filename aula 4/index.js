207f925ed897bcbcf9cccf79475168650370c22a
MATOSISA1223 <isabella.nascimento42@etec.sp.gov.br>
2025-04-22 18:12:21 -0300

    aula202205- Parte 1 Deposito

--- aula 4/index.js
+++ aula 4/index.js
@@ -1,34 +1,40 @@
 const inquirer = require('inquirer')
-const chalk = require
+const chalk = require('chalk')
+
 
 const fs = require('fs')
- function operation(){
- inquirer.prompt([
-    {
-        type: 'list',
-        name: 'action',
-        messagem: 'escolha duas opcoes no menu ',
-        choices: [
-            'criar conta',
-            'consultar saldo',
-            'depositar',
-            'sacar',
-            'sair',
-        ],
-    }
-    .then ((answer) => {
+operation()
+function operation() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'O que deseja fazer?',
+            choices: [
+                'Criar conta',
+                'Consultar saldo',
+                'Depositar',
+                'Sacar',
+                'Sair',
+            ],
+        }
+    ]).then((answer) => {
         const action = answer['action']
-        if(action === 'Criar conta'){
-        console. log ( 'Criando a conta...'){
 
-      
 
-        }else if(action === 'Consultar saldo'){
-           console.log('consultando saldo') 
-        }else if(action ==='depositar'){
-            console.log('depositando')
-        }else if(action === 'Consultar saldo'){
-       
-})
-
+        if (action === 'Criar conta') {
+            createAccount()
+        } else if (action === 'Depositar') {
+            console.log(action)
+        } else if (action === 'Consultar saldo') {
+            console.log(action)
+        } else if (action === 'Sacar') {
+            console.log(action)
+        } else if (action === 'Sair') {
+            console.log('Saindo do sistema...')
+            process.exit()
+        } else {
+            console.log('Nao e valido')
+        }
+       })
 }
